a7321e8c8bf9dcfb2910f09c8f65004a88b6bee8
igor-voronezhtsev <i.voronezhtsev@gmail.com>
2025-12-16 21:19:53 +0300

    Optimize Imports

--- app/src/main/java/com/example/workmatecountries/MainActivity.kt
+++ app/src/main/java/com/example/workmatecountries/MainActivity.kt
@@ -4,7 +4,6 @@ import CountryScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.workmatecountries.data.CountriesRepository
 import com.example.workmatecountries.domain.usecases.GetCountriesUseCase
-import com.example.workmatecountries.ui.CountriesViewModel
 import com.example.workmatecountries.ui.theme.WorkMateCountriesTheme
 
 class MainActivity : ComponentActivity() {
