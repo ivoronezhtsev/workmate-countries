35c5fbdd2f9ef409b157e005f2235b12b05dc67e
igor-voronezhtsev <i.voronezhtsev@gmail.com>
2025-12-16 21:24:09 +0300

    Delete comments

--- app/src/main/java/com/example/workmatecountries/ui/CountriesUiState.kt
+++ app/src/main/java/com/example/workmatecountries/ui/CountriesUiState.kt
@@ -3,7 +3,7 @@ package com.example.workmatecountries.ui
 import com.example.workmatecountries.domain.models.Country
 
 sealed class CountriesUiState {
-    object Loading : CountriesUiState()                // экран загружается
-    data class Success(val countries: List<Country>) : CountriesUiState() // данные загружены
-    data class Error(val message: String) : CountriesUiState()           // произошла ошибка
+    object Loading : CountriesUiState()
+    data class Success(val countries: List<Country>) : CountriesUiState()
+    data class Error(val message: String) : CountriesUiState()
 }
