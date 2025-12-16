3232e389698d51a95bca2ccda08c07d7fc8ebbe9
igor-voronezhtsev <i.voronezhtsev@gmail.com>
2025-12-17 01:51:39 +0300

    Удалил лишний код во MainActivity.kt, Центрирование индикатора загрузки

--- app/src/main/java/com/example/workmatecountries/MainActivity.kt
+++ app/src/main/java/com/example/workmatecountries/MainActivity.kt
@@ -5,13 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.workmatecountries.data.CountriesRepository
 import com.example.workmatecountries.domain.usecases.GetCountriesUseCase
-import com.example.workmatecountries.ui.theme.WorkMateCountriesTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -25,19 +20,4 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WorkMateCountriesTheme {
-        Greeting("Android")
-    }
-}
 
--- app/src/main/java/com/example/workmatecountries/ui/CountryScreen.kt
+++ app/src/main/java/com/example/workmatecountries/ui/CountryScreen.kt
@@ -1,3 +1,5 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -31,7 +34,12 @@ fun CountryScreen(getCountriesUseCase: GetCountriesUseCase) {
 
     when (uiState) {
         is CountriesUiState.Loading -> {
-            CircularProgressIndicator()
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
         }
 
         is CountriesUiState.Success -> {
