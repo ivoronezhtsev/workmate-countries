902b3113e1db3b5a2b952eaf41147b0902105b73
igor-voronezhtsev <i.voronezhtsev@gmail.com>
2025-12-17 21:32:20 +0300

    Небольшие правки

--- app/src/main/java/com/example/workmatecountries/ui/countries/CountriesScreen.kt
+++ app/src/main/java/com/example/workmatecountries/ui/countries/CountriesScreen.kt
@@ -19,10 +19,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.workmatecountries.R
 import com.example.workmatecountries.domain.usecases.GetCountriesUseCase
 import com.example.workmatecountries.ui.countries.CountriesUiState
 import com.example.workmatecountries.ui.countries.CountriesViewModel
@@ -48,7 +50,7 @@ fun CountriesScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Countries") }
+                title = { Text(stringResource(R.string.app_name)) }
             )
         }
     ) { paddingValues ->
